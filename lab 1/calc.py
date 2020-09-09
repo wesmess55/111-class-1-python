66725b205209ae20d0e07ad183c98f61f8827c45
wesmess55 <66344929+wesmess55@users.noreply.github.com>
2020-09-08 21:20:13 -0500

    Add files via upload

---
+++ lab 1/calc.py
@@ -0,0 +1,33 @@
+def print_separator():
+    print('--------------')
+
+
+
+def print_menu():
+    print('[1] Add')
+    print('[2] Subtract')
+    print('[3] Multiply')
+    print('[4] Divide')
+
+
+print_menu()
+
+opc = input('Please choose an option: ')
+
+
+num1 = float (input('First number: '))
+num2 = float (input('Second number: '))
+
+    
+if(opc =='1'):    
+    res = num1 + num2
+    print('Res: '+ str(res))
+elif (opc =='2'):
+    res= num1 - num2
+    print('Res:' + str(res))
+elif(opc == '3'):
+    res= num1 * num2
+    print('Res:' + str(res))
+elif(opc == '4'):
+    res= num1 / num2
+    print('Res:' + str(res))
